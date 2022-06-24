53f22663799415cd41f73bf16b2e975937a6a031
Aspirin Shi <aspirin2d@outlook.com>
2022-06-24 15:02:46 +0800

    add pugs

--- views/js/index.js
+++ views/js/index.js
@@ -1,1 +1,9 @@
-console.log("Hello, World")
+import MicroModal from "micromodal";
+MicroModal.init({
+  onShow: (modal) => console.info(`${modal.id} is shown`),
+  debugMode: true,
+});
+
+$("#new").on("click", function (_) {
+  MicroModal.show("modal-new");
+});
